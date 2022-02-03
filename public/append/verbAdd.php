0790018f915abcdd2cbafcb00a7d6f9b63002b72
Yasin Şahin <hacklojen01@gmail.com>
2022-02-03 12:13:18 +0300

    verb add

--- public/append/verbAdd.php
+++ public/append/verbAdd.php
@@ -55,7 +55,15 @@ session_start();
                 <td id="tanım">Fiil Kontrol :</td>
                 <td id="tanım2"><input type="text" name="verb"></td>
                 <td id="tanım3"><input type="submit" name="gonder" id="refresh" value=""></td>
-                <td id="tanım4"><?php echo $_SESSION['controlverb']?></td>
+                <td id="tanım4">
+                    <?php
+                        if($_SESSION['controlverb'] =="var"):
+                            echo "<img src='../image/onay.png'>";
+                        else:
+                            echo "<img src='../image/hata.png'>";
+                        endif;
+                    ?>
+                </td>
             </tr>
         </form>
     </table>
@@ -66,54 +74,57 @@ session_start();
         <form action="../php/add_verb.php" method="post" enctype="multipart/form-data">
         <tr>
             <td id="tanım">Verb 1 :</td>
-            <td id="tanım5"><input type="text" name="verb1" placeholder="1. Hali"></td>
-            <td rowspan="2" id="tanım6"> <?php echo $_SESSION['addverb']?></td>
+            <td id="tanım9"><input type="text" name="verb1" placeholder="1. Hali"></td>
+            <td id="tanım9"><input type="text" name="verb2" placeholder="2. Hali"></td>
+            <td id="tanım9"><input type="text" name="verb3" placeholder="3. Hali"></td>
+            <td rowspan="2" id="tanım6">
+                <?php
+                if($_SESSION['addverb'] =="ok"):
+                    echo "<img src='../image/onay.png'>";
+                else:
+                    echo "<img src='../image/hata.png'>";
+                endif;
+                ?>
+            </td>
         </tr>
-        <tr>
-            <td id="tanım">Verb 2 :</td>
-            <td id="tanım5"><input type="text" name="verb2" placeholder="2. Hali"></td>
-        </tr>
-        <tr>
-            <td id="tanım">Verb 3 :</td>
-            <td id="tanım5"><input type="text" name="verb3" placeholder="3. Hali"></td>
-            <td id="tanım6" rowspan="9"><input type="submit" name="gonder" id="add" value=""></td>
-        </tr>
+
         <tr>
             <td id="tanım">Translate :</td>
-            <td id="tanım5"><input type="text" name="translate" placeholder="Çevirisi"></td>
+            <td id="tanım5" colspan="3"><input type="text" name="translate" placeholder="Çevirisi"></td>
         </tr>
         <tr>
             <td id="tanım">Type :</td>
-            <td id="tanım5">
+            <td id="tanım5" colspan="3">
                 <select name="verbtype" id="combo">
                     <option value="regular" name="regular">Regular</option>
                     <option value="irregular" name="irregular">İrregular</option>
                 </select>
             </td>
+            <td id="tanım6" rowspan="9"><input type="submit" name="gonder" id="add" value=""></td>
         </tr>
         <tr>
             <td id="tanım">Example :</td>
-            <td id="tanım5"><input type="text" name="ex1" placeholder="Örnek 1"></td>
+            <td id="tanım5" colspan="3"><input type="text" name="ex1" placeholder="Örnek 1"></td>
         </tr>
         <tr>
             <td id="tanım">Ex. Translate :</td>
-            <td id="tanım5"><input type="text" name="ex1t" placeholder="Örnek 1 Çevirisi"></td>
+            <td id="tanım5" colspan="3"><input type="text" name="ex1t" placeholder="Örnek 1 Çevirisi"></td>
         </tr>
         <tr>
             <td id="tanım">Example :</td>
-            <td id="tanım5"><input type="text" name="ex2" placeholder="Örnek 2"></td>
+            <td id="tanım5" colspan="3"><input type="text" name="ex2" placeholder="Örnek 2"></td>
         </tr>
         <tr>
             <td id="tanım">Ex. Translate :</td>
-            <td id="tanım5"><input type="text" name="ex2t" placeholder="Örnek 2 Çevirisi"></td>
+            <td id="tanım5" colspan="3"><input type="text" name="ex2t" placeholder="Örnek 2 Çevirisi"></td>
         </tr>
             <tr>
                 <td id="tanım">Example :</td>
-                <td id="tanım5"><input type="text" name="ex3" placeholder="Örnek 3"></td>
+                <td id="tanım5" colspan="3"><input type="text" name="ex3" placeholder="Örnek 3"></td>
             </tr>
             <tr>
                 <td id="tanım">Ex. Translate :</td>
-                <td id="tanım5"><input type="text" name="ex3t" placeholder="Örnek 3 Çevirisi"></td>
+                <td id="tanım5" colspan="3"><input type="text" name="ex3t" placeholder="Örnek 3 Çevirisi"></td>
             </tr>
 
         </form>
